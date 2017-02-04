20e329ee018e506478427e96af90d6778d63b1c1
Daniel <danielems3@outlook.com>
2017-02-04 08:32:56 -0500

    Create a .h

---
+++ stock.h
@@ -0,0 +1,41 @@
+#ifndef STOCK_H
+
+#define STOCK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sysexits.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+
+
+enum {LINE_SIZE = 256};
+enum {MAX_CO_SIZE = 64};
+enum {MAX_TICKER_LEN = 6};
+
+//Code modified from BST code 
+typedef struct stock{
+    int cost;
+	char *company;
+	char ticker[6];
+
+    struct stock *left;
+    struct stock *right;
+} stock;
+
+stock *Insert(stock *root, int cost, char *company, char *ticker);
+stock *rightRotate(stock *root);
+stock *leftRotate(stock *root);
+stock *search(stock *root, char *ticker);
+stock *splay(stock *root, char *ticker);
+void maff(stock *tree, int cost);
+void print_node(stock *root);
+void destroy_stocks (stock * tree);
+#endif
