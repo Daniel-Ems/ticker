988657b2c140a45ebe1d44ad89e480bf9c64cae6
Daniel <danielems3@outlook.com>
2017-02-04 19:34:16 -0500

    final

--- stock.c
+++ stock.c
@@ -1,7 +1,6 @@
 #include "stock.h"
 
-
-// Code taken from intersector, used to organize tree alphabetically.
+//inserts by tickers
 stock *Insert(stock *root, int cost, char *company, char *ticker)
 {
   int result;
@@ -16,7 +15,7 @@ stock *Insert(stock *root, int cost, char *company, char *ticker)
 	}
       else
 	{
-	
+		//Only calloc room for company if there is a company name. 
 		if(company == NULL)
 		{
 			root -> company = NULL;
@@ -35,6 +34,8 @@ stock *Insert(stock *root, int cost, char *company, char *ticker)
 
   else
     {
+	  //strcasecmp is used and stored in result to sort by ticker and 
+	  //reduce the number of times the function is calle. 
       result = strcasecmp (ticker, root->ticker);
       if (result < 0)
 	{
@@ -53,345 +54,7 @@ stock *Insert(stock *root, int cost, char *company, char *ticker)
   return root;
 }
 
-
-stock *rightRotate(stock *root)
-{
-	stock *temp = root->left;
-	root->left = temp->right;
-	temp->right = root;
-	return temp;
-}
-
-stock *leftRotate(stock *root)
-{
-	stock *temp = root->right;
-	root->right = temp->left;
-	temp->left = root;
-	return temp;
-}
-
-
-stock *search(stock *root, char *ticker)
-{
-
-    // Base cases: root is NULL or key is present at root **
-    if (root == NULL)
-	{
-        return root;
-	}
-	
-	int result = strcasecmp(ticker, root->ticker);
-	if (result == 0)
-	{
-		return root;
-	}
- 
-    // Key lies in left subtree **
-    if (result < 0)
-    {
-        // Key is not in tree, we are done
-        if (root->left == NULL) 
-		{
-			return root;
-		}
- 
-        // Zig-Zig (Left Left)
-		result = strcasecmp(ticker, root->left->ticker);
-        if (result < 0)
-        {
-            // First recursively bring the key as root of left-left
-            root->left->left = search(root->left->left, ticker);
- 
-            // Do first rotation for root, second rotation is done after else
-            root = rightRotate(root);
-        }
-        else if (result > 0) // Zig-Zag (Left Right)
-        {
-            // First recursively bring the key as root of left-right
-            root->left->right = search(root->left->right, ticker);
- 
-            // Do first rotation for root->left
-            if (root->left->right != NULL)
-				{
-                root->left = leftRotate(root->left);
-				}
-        }
- 
-        // Do second rotation for root
-        return (root->left == NULL) ? root: rightRotate(root);
-    }
-    else // Key lies in right subtree
-    {
-        // Key is not in tree, we are done
-        if (root->right == NULL)
-		{
-		
-			return root;
-		} 
-
-		result = strcasecmp(ticker, root->right->ticker);
-        // Zag-Zig (Right Left)
-        if (result < 0)
-        {
-            // Bring the key as root of right-left
-            root->right->left = search(root->right->left, ticker);
- 
-            // Do first rotation for root->right
-            if (root->right->left != NULL)
-                root->right = rightRotate(root->right);
-        }
-        else if (result > 0)// Zag-Zag (Right Right)
-        {
-            // Bring the key as root of right-right and do first rotation
-            root->right->right = search(root->right->right, ticker);
-            root = leftRotate(root);
-        }
- 
-        // Do second rotation for root
-        return (root->right == NULL) ? root: leftRotate(root);
-    }
-}
-
-int ticker_check(char *token)
-{
-	int i=0;
-	int flag;
-	if(token == NULL)
-	{
-		printf("Bad ticker\n");
-		return flag = 0;	
-	}
-	if(!isalpha(token[i]))
-	{
-		return flag = 0;
-	}
-	
-	return flag = 1;
-}
-
-int input_cash(char *token)
-{
-	int flag;
-	size_t index = 0;
-	if(token == NULL)
-	{
-		printf("%s - stock ignored\n", token);
-		return flag = 0;
-	}
-	if(token[0] == '-')
-	{
-		printf("%s - stock ignored\n", token);
-		return flag = 0;
-	}
-	if(strlen(token) > MAX_DIGITS)
-	{
-		printf("%s - stock ignored\n", token);
-		return flag = 0;
-	}
-	for(index = 0; index < strlen(token); index++)
-	{
-		if(!isdigit(token[index]))
-		{
-			printf("%s - stock ignored\n", token);
-			return flag = 0;
-		}
-	}
-	return flag = 1;
-}
-
-int input_cents (char *token)
-{
-	int flag;
-	size_t index = 0;
-	if(token == NULL)
-	{
-		printf("%s - stock ignored\n", token);
-		return flag = 0;
-	}
-	if(strlen(token) < MAX_CENTS)
-	{
-		printf("NOT enough sense\n");
-		return flag = 0;
-	}
-	for(index = 0; index < strlen(token); index++)
-	{
-		if(!isdigit(token[index]))
-		{
-			printf("%s - stock ignored\n", token);
-			return flag = 0;
-		}
-	}
-	return flag = 1;
-}
-
-char *two_cents(char *token)
-{
-	if(strlen(token) > MAX_CENTS)
-	{
-		token[MAX_CENTS] = '\0';
-	}
-	
-	return token;
-}
-
-int price_check(char *token)
-{
-	size_t index = 0;
-	size_t value = 0;
-	int flag = 0;
-	if(token == NULL)
-	{
-		printf("It's not difficult, just get it right.\n");
-		return flag = 0;
-	}
-
-	if(token[0] == '-')
-	{
-		if(strlen(token) > MAX_DIGITS+1)
-		{
-			printf("The cost can only be 7 digits\n");
-			return flag = 0;
-		}		
-		value = 1;
-	}
-
-	else
-	{
-		if(strlen(token) > MAX_DIGITS)
-		{
-			printf("The cost can only be 7 digits\n");
-			return flag = 0;
-		}	
-		value = 0;
-	}
-
-	for(index = value; index <strlen(token); index++)
-	{
-		if(!isdigit(token[index])) 
-		{
-			printf("You messed up\n");
-			return flag = 0;
-		}
-	}
-	return flag = 1;
-}
-
-int cent_check(char *token)
-{
-	size_t index = 0;
-	int flag = 0;
-
-	if(token == NULL)
-	{
-		printf("It's not difficult, just get it right.\n");
-		return flag = 0;
-	}
-	if(strlen(token) < MAX_CENTS)
-	{
-		printf("NOT ENOUGH CENTS\n");
-		return flag = 0;
-	}
-	for(index = 0; index <strlen(token); index++)
-	{
-		if(!isdigit(token[index])) 
-		{
-			printf("So close! But close is only good for horse shoes "
-					"and hand grenades, try again\n");
-			return flag = 0;
-			
-		}
-	}
-	return flag = 1;
-}
-
-void maff(stock *tree, int cost)
-{
-	if(cost < 0)
-	{
-		if(tree->cost + cost < 01)
-		{
-			printf("You have subtracted too much, operation ignored\n");
-			return;
-		}
-		tree->cost += cost;
-	}
-	else 
-	{
-		if(tree->cost + cost > 100000000)
-		{
-			printf("You have added too much, operation ignored\n");
-			return;
-		}
-		tree->cost += cost;
-	}
-}
-
-void print_node(stock *root)
-{
-
-	if(root->left)
-	{
-		print_node(root->left);
-	}
-	float divide = (float)root->cost / 100;
-	printf("%s %.2f %s\n", root->ticker, divide, root->company);
-	if (root -> right)
-	{
-		print_node(root->right);
-	}
-	
-}
-
-void
-destroy_stocks (stock * tree)
-{
-  if (tree == NULL)
-    {
-      return;
-    }
-  destroy_stocks (tree->left);
-  destroy_stocks (tree->right);
-  free (tree->company);
-  free (tree);
-}
-
-	
-size_t tree_size(stock *root)
-{
-	size_t count = 1;
-	if(!root)
-	{
-		return 0;
-	}
-	count += tree_size(root->left);
-	count += tree_size(root->right);
-	
-	return count;
-}
-
-void traverse(stock *tree)
-{
-	if(tree == NULL)
-	{
-		return;
-	}
-	traverse(tree->left);
-	traverse(tree->right);
-}
-stock *new_tree(stock *tree, stock *second, void(*traverse)(stock*),
-		 stock*(*Insert_num)(stock *, int, char *, char *)) 
-	{
-		if(tree == NULL)
-		{
-			return second;
-		}
-		traverse(tree);
-		second = Insert_num(second, tree->cost, tree->company, tree->ticker);
-		traverse(tree);
-		return second;
-		
-	}
-
+//Inserts by cost
 stock *Insert_num(stock *root, int cost, char *company, char *ticker)
 {
   
@@ -430,7 +93,7 @@ stock *Insert_num(stock *root, int cost, char *company, char *ticker)
 	{
 	  root->left = Insert_num(root->left, cost, company, ticker);
 	}
-      else if (cost > root->cost)
+      else if (cost >= root->cost)
 	{
 	  root->right = Insert_num(root->right, cost, company, ticker);
 	}
@@ -442,5 +105,352 @@ stock *Insert_num(stock *root, int cost, char *company, char *ticker)
     }
   return root;
 }
+
+//Assigns the root to the right child of it's left childs node, then moves 
+//the child to root.
+stock *rightRotate(stock *root)
+{
+	stock *temp = root->left;
+	root->left = temp->right;
+	temp->right = root;
+	return temp;
+}
+
+//Assigns the root to the left child of it's right childs node, then moves 
+//the child to root.
+stock *leftRotate(stock *root)
+{
+	stock *temp = root->right;
+	root->right = temp->left;
+	temp->left = root;
+	return temp;
+}
+
+
+stock *search(stock *root, char *ticker)
+{
+
+    //The base case
+    if (root == NULL)
+	{
+        return root;
+	}
+	//our compare stored to limit number of times the function is called
+	int result = strcasecmp(ticker, root->ticker);
+	if (result == 0)
+	{
+		return root;
+	}
+ 
+    //The ticker is found in the left subtree
+    if (result < 0)
+    {
+        //ticker is not present so we begin return
+        if (root->left == NULL) 
+		{
+			return root;
+		}
+ 
+        //Zig-Zig (Left Left)
+		result = strcasecmp(ticker, root->left->ticker);
+        if (result < 0)
+        {
+            // First recursively bring the ticker as root of left-left
+            root->left->left = search(root->left->left, ticker);
+ 
+            // Do first rotation for root, second rotation is done after else
+            root = rightRotate(root);
+        }
+        else if (result > 0) // Zig-Zag (Left Right)
+        {
+            // First recursively bring the ticker as root of left-right
+            root->left->right = search(root->left->right, ticker);
+ 
+            // Do first rotation for root->left
+            if (root->left->right != NULL)
+				{
+                root->left = leftRotate(root->left);
+				}
+        }
+ 
+        // Do second rotation for root
+        return (root->left == NULL) ? root: rightRotate(root);
+    }
+    else //ticker lies in right subtree
+    {
+        //ticker is not in tree, we are done
+        if (root->right == NULL)
+		{
+		
+			return root;
+		} 
+
+		result = strcasecmp(ticker, root->right->ticker);
+        // Zag-Zig (Right Left)
+        if (result < 0)
+        {
+            // Bring the ticker as root of right-left
+            root->right->left = search(root->right->left, ticker);
+ 
+            // Do first rotation for root->right
+            if (root->right->left != NULL)
+                root->right = rightRotate(root->right);
+        }
+        else if (result > 0)// Zag-Zag (Right Right)
+        {
+            // Bring the ticker as root of right-right and do first rotation
+            root->right->right = search(root->right->right, ticker);
+            root = leftRotate(root);
+        }
+ 
+        // Do second rotation for root
+        return (root->right == NULL) ? root: leftRotate(root);
+    }
+}
+
+int ticker_check(char *token)
+{
+	int i=0;
+	int flag;
+	//Nothing was strtok'd.
+	if(token == NULL)
+	{
+		return flag = 0;	
+	}
+	//The first charcter is not a letter
+	if(!isalpha(token[i]))
+	{
+		return flag = 0;
+	}
+	//The ticker meets the requirements. 
+	return flag = 1;
+}
+
+//Error handling  for file i/o dollars
+int input_cash(char *token)
+{
+	int flag;
+	size_t index = 0;
+	size_t value = 0;
+
+	//Nothing was strtok'd
+	if(token == NULL)
+	{
+		return flag = 0;
+	}
+	//No opening stock can not be negative
+	if(token[0] == '-')
+	{
+		return flag = 0;
+	}
+	if(token[0] == '+')
+	{
+		//move starting point for isdigit check
+		value = 1;
+	}
+	//length check
+	if(strlen(token) > MAX_DIGITS+value)
+	{
+		return flag = 0;
+	}
+	//isdigit check
+	for(index = value; index < strlen(token); index++)
+	{
+		if(!isdigit(token[index]))
+		{
+			return flag = 0;
+		}
+	}
+	//all requirements were met
+	return flag = 1;
+}
+
+//Error handling for file i/o cents 
+int input_cents (char *token)
+{
+	int flag;
+	size_t index = 0;
+	//Nothing was strtok'd
+	if(token == NULL)
+	{
+		return flag = 0;
+	}
+	//length check
+	if(strlen(token) < MAX_CENTS)
+	{
+		return flag = 0;
+	}
+	//is digit check
+	for(index = 0; index < strlen(token); index++)
+	{
+		if(!isdigit(token[index]))
+		{
+			return flag = 0;
+		}
+	}
+	//Passes all requirements
+	return flag = 1;
+}
+
+//Cuts off any cents beyond two digits
+char *two_cents(char *token)
+{
+	if(strlen(token) > MAX_CENTS)
+	{
+		token[MAX_CENTS] = '\0';
+	}
+	
+	return token;
+}
+
+//Error handling or user dollar i/o
+int price_check(char *token)
+{
+	size_t index = 0;
+	size_t value = 0;
+	int flag = 0;
+	//Nothing was strtok'd
+	if(token == NULL)
+	{
+		return flag = 0;
+	}
+	//Check if negative 
+	if(token[0] == '-')
+	{
+		if(strlen(token) > MAX_DIGITS+1)
+		{
+			return flag = 0;
+		}	
+		//Adjust isdigit start	
+		value = 1;
+	}
+
+	else
+	{
+		//length check
+		if(strlen(token) > MAX_DIGITS)
+		{
+			return flag = 0;
+		}	
+		value = 0;
+	}
+	//Isdigit check
+	for(index = value; index <strlen(token); index++)
+	{
+		if(!isdigit(token[index])) 
+		{
+			return flag = 0;
+		}
+	}
+	return flag = 1;
+}
+
+//Error handling for user cents i/o
+int cent_check(char *token)
+{
+	size_t index = 0;
+	int flag = 0;
+	//Nothing was strtok'd
+	if(token == NULL)
+	{
+		return flag = 0;
+	}
+	//length check
+	if(strlen(token) < MAX_CENTS)
+	{
+		return flag = 0;
+	}
+	//isdigit check
+	for(index = 0; index <strlen(token); index++)
+	{
+		if(!isdigit(token[index])) 
+		{
+			return flag = 0;
+			
+		}
+	}
+	//All requirements were met 
+	return flag = 1;
+}
+
+void maff(stock *tree, int cost)
+{
+	//cost is negative
+	if(cost < 0)
+	{
+		//limit to > 0.00
+		if(tree->cost + cost < 01)
+		{
+			printf("You have subtracted too much, operation ignored\n");
+			return;
+		}
+		tree->cost += cost;
+	}
+	//cost is positive
+	else 
+	{	
+		//limit to 1,000,000
+		if(tree->cost + cost > 100000000)
+		{
+			printf("You have added too much, operation ignored\n");
+			return;
+		}
+		tree->cost += cost;
+	}
+}
+
+void print_node(stock *root)
+{
+	if(root==NULL)
+	{
+		return;
+	}
+
+	if(root->left)
+	{
+		print_node(root->left);
+	}
+	//return price to decimal value
+	float divide = (float)root->cost / 100;
+	printf("%s %.2f %s\n", root->ticker, divide, root->company);
+	if (root -> right)
+	{
+		print_node(root->right);
+	}
+	
+}
+
+//Free's the trees
+void
+destroy_stocks (stock * tree)
+{
+  if (tree == NULL)
+    {
+      return;
+    }
+  destroy_stocks (tree->left);
+  destroy_stocks (tree->right);
+  free (tree->company);
+  free (tree);
+}
+
+//Used to build out new tree 
+stock *new_tree(stock *tree, stock *second,
+		 stock*(*Insert_num)(stock *, int, char *, char *)) 
+	{
+		if(tree == NULL)
+		{
+			return tree;
+		}
+		//Insert value of current tree into new tree 
+		second = Insert_num(second, tree->cost, tree->company, tree->ticker);
+		//move through the current tree
+		new_tree(tree->left, second, Insert_num);
+		new_tree(tree->right, second, Insert_num);
+		return second;
+	}
+
+
+
  
 
